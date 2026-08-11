c8a4f26173e9ed594a07de4182307422205a55ab
Manjeet Kumar Jha <manjeetshandilya16@gmail.com>
2026-08-11 21:41:35 +0530

    Url Update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,8 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-del-backend-1-fsxw.onrender.com";
-  
+  const url = "https://food-del-backend-1-fsxw.onrender.com";  
   // const url = "http://localhost:4000";
   const [token, setToken] = useState("")
   const [food_list, setFoodList] = useState([])
